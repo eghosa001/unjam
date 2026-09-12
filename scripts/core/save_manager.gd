1feae80b9d2c0e3910ac0080758daa6fbb6e3309
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-12 20:39:17 +0100

    feat: add perfect streaks prestige milestone chests and world badges

--- scripts/core/save_manager.gd
+++ scripts/core/save_manager.gd
@@ -18,7 +18,13 @@ const DEFAULT_DATA := {
 	"undos_used": 0,
 	"remove_ads": false,
 	"total_levels_completed": 0,
-	"total_rescues": 0
+	"total_rescues": 0,
+	"perfect_clears": 0,
+	"perfect_streak": 0,
+	"best_perfect_streak": 0,
+	"milestone_chests": [],
+	"world_badges": [],
+	"prestige_points": 0
 }
 
 var data: Dictionary = DEFAULT_DATA.duplicate(true)
@@ -51,7 +57,8 @@ func save() -> void:
 	if file != null:
 		file.store_string(JSON.stringify(data))
 
-func complete_level(level_number: int, stars: int, rescue_id: String, coin_reward: int = 25) -> void:
+func complete_level(level_number: int, stars: int, rescue_id: String, coin_reward: int = 25) -> Dictionary:
+	var rewards := {"perfect": false, "milestone": false, "world_badge": false, "bonus_coins": 0, "prestige": 0}
 	var key := str(level_number)
 	var previous_stars := int(data.stars.get(key, 0))
 	data.stars[key] = max(previous_stars, stars)
@@ -61,7 +68,41 @@ func complete_level(level_number: int, stars: int, rescue_id: String, coin_rewar
 	if not rescue_id.is_empty() and not rescue_id in data.rescued:
 		data.rescued.append(rescue_id)
 		data.total_rescues = int(data.total_rescues) + 1
+
+	if stars == 3:
+		rewards.perfect = true
+		data.perfect_clears = int(data.perfect_clears) + 1
+		data.perfect_streak = int(data.perfect_streak) + 1
+		data.best_perfect_streak = max(int(data.best_perfect_streak), int(data.perfect_streak))
+		if int(data.perfect_streak) % 5 == 0:
+			rewards.bonus_coins = 50
+			rewards.prestige = 1
+			data.coins = int(data.coins) + 50
+			data.prestige_points = int(data.prestige_points) + 1
+	else:
+		data.perfect_streak = 0
+
+	if level_number % 10 == 0:
+		var chest_key := str(level_number)
+		if not chest_key in data.milestone_chests:
+			data.milestone_chests.append(chest_key)
+			rewards.milestone = true
+			rewards.bonus_coins = int(rewards.bonus_coins) + 100
+			data.coins = int(data.coins) + 100
+
+	if level_number % 100 == 0:
+		var world := int(level_number / 100)
+		var badge_key := str(world)
+		if not badge_key in data.world_badges:
+			data.world_badges.append(badge_key)
+			rewards.world_badge = true
+			rewards.prestige = int(rewards.prestige) + 5
+			data.prestige_points = int(data.prestige_points) + 5
+			data.coins = int(data.coins) + 250
+			rewards.bonus_coins = int(rewards.bonus_coins) + 250
+
 	save()
+	return rewards
 
 func get_stars(level_number: int) -> int:
 	return int(data.stars.get(str(level_number), 0))
