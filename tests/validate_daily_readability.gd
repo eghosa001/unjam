5734fefd6aa07ca441d1e707528a17b07210356b
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-21 20:14:06 +0100

    Match stable Daily readability test hooks

--- tests/validate_daily_readability.gd
+++ tests/validate_daily_readability.gd
@@ -14,9 +14,9 @@ func _run() -> void:
 	await _frames(3)
 
 	for game_id in ["rescue_rush", "water_sort", "block_puzzle"]:
-		var title := main.find_child("DailyTitle/%s" % game_id, true, false) as Label
+		var title := main.find_child("DailyTitle_%s" % game_id, true, false) as Label
 		var play := main.find_child("DailyPlay/%s" % game_id, true, false) as Button
-		var status := main.find_child("DailyProgressLabel/%s" % game_id, true, false) as Label
+		var status := main.find_child("DailyProgressLabel_%s" % game_id, true, false) as Label
 		if title == null or title.get_theme_font_size("font_size") < 18:
 			return _fail("Daily title is too small for %s" % game_id)
 		if title.get_theme_constant("outline_size") < 2:
