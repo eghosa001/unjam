7589fc2b0c7bae33119e81526c6e0dd0c86cd2e5
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-21 21:08:15 +0100

    Guard secondary full-name nav spacing

--- tests/validate_collection_daily_value.gd
+++ tests/validate_collection_daily_value.gd
@@ -63,6 +63,8 @@ func _run() -> void:
 			failures.append("Home is missing Daily Games route token: %s" % token)
 	for token in [
 		"func build_daily_games",
+		"\"collection\":222.0, \"settings\":310.0",
+		"nav_label.clip_text = true",
 		"DailyCard/",
 		"PLAY TODAY",
 		"DONE TODAY",
