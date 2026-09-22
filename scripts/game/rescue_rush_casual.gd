f0e7f250ab6d910e09ced2ce06bf796691ff590b
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-22 08:57:30 +0100

    fix Rescue chain status suffix reference

--- scripts/game/rescue_rush_casual.gd
+++ scripts/game/rescue_rush_casual.gd
@@ -366,7 +366,7 @@ func render_board() -> void:
 		if objective_type == "perfect_rescue":
 			moves_label.text = "MOVES %d/%d   •   LIVES %s%s" % [moves, action_budget, lives_text, chain_suffix]
 		else:
-			moves_label.text = "MOVES %d   •   3★≤%d   •   LIVES %s%s" % [moves, par_moves, lives_text, chain_text]
+			moves_label.text = "MOVES %d   •   3★≤%d   •   LIVES %s%s" % [moves, par_moves, lives_text, chain_suffix]
 		moves_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		moves_label.add_theme_font_size_override("font_size",13)
 	if rescue_label != null:
