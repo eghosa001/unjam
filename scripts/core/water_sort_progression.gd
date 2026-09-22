743b701e176a44c25595440b59ae2ee745ec0ca1
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-22 10:47:41 +0100

    Smooth early Water difficulty handoff

--- scripts/core/water_sort_progression.gd
+++ scripts/core/water_sort_progression.gd
@@ -68,7 +68,7 @@ static func profile(raw_level: int) -> Dictionary:
 		# retaining the normal sawtooth relief/challenge rhythm.
 		var early_progress: float = float(level - 11) / 89.0
 		var early_base: int = roundi(lerpf(25.0, 34.0, early_progress))
-		var early_bonus: int = int([0, 2, 4, 6, 8, 10][clampi(rank, 0, 5)])
+		var early_bonus: int = int([0, 1, 2, 3, 4, 5][clampi(rank, 0, 5)])
 		target_moves = clampi(early_base + early_bonus, 25, 44)
 		var early_score_base: int = roundi(lerpf(50.0, 55.0, early_progress))
 		var early_score_bonus: int = int([0, 2, 4, 6, 8, 10][clampi(rank, 0, 5)])
